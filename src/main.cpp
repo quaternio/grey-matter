507650f66d4ae93ccacf5c0ff2b50d89f59c3d01
Thomas Noel <tnoel@novadynamics.com>
2023-10-17 15:54:23 -0700

    Implement inner product to get feet wet

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 
+using namespace std;
+
+void innerProduct(float* x, float* y, float* out) {
+   int N = sizeof(x) / sizeof(x[0]);
+   int ySize = sizeof(y) / sizeof(y[0]);
+   int outSize = sizeof(out) / sizeof(out[0]);
+
+   if (ySize == N && outSize == N) {
+       for (int i = 0; i < N; i++) {
+          *out += x[i] * y[i]; 
+       }
+   }
+}
+
+//float* matmul(float* A, float* x) {
+//   int N = sizeof(A);
+//   float outVec [N];
+//
+//   if (N > 0) {
+//       int D = sizeof(A[0]);
+//
+//       // TODO 
+//   }
+//
+//   return &outVec;
+//}
+
 int main() {
-    std::cout << "Hello world!\n" << std::endl;
+    float a [3] = { 1.7, 2.2 };
+    float b [3] = { 1.0, 1.0 };
+    float c = 0;
+
+    innerProduct(a, b, &c);
+
+    cout << c << endl;
     
     return 0;
 }
