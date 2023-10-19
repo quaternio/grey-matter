3758a8fd4b626cb3075e861e4453cabc893638f6
Thomas Noel <tnoel@novadynamics.com>
2023-10-19 14:53:16 -0700

    Clean up inner product; fix array pointer function scope bug

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,24 @@
 
 using namespace std;
 
-void innerProduct(float* x, float* y, float* out) {
-   int N = sizeof(x) / sizeof(x[0]);
-   int ySize = sizeof(y) / sizeof(y[0]);
-   int outSize = sizeof(out) / sizeof(out[0]);
+void innerProduct(float* x, float* y, float* out, int* dim)  {
+    // We pass in dim because sizeof(x) / sizeof(x[0]) doesn't work here..
+    // x is construed as the pointer pointing to x[0] in function scope
+    int N = *dim;
 
-   if (ySize == N && outSize == N) {
-       for (int i = 0; i < N; i++) {
-          *out += x[i] * y[i]; 
-       }
-   }
+    for (int i = 0; i < N; i++) {
+        cout << *out << endl;
+        *out += x[i] * y[i]; 
+    }
 }
 
-//float* matmul(float* A, float* x) {
-//   int N = sizeof(A);
-//   float outVec [N];
-//
-//   if (N > 0) {
-//       int D = sizeof(A[0]);
-//
-//       // TODO 
-//   }
-//
-//   return &outVec;
-//}
-
 int main() {
-    float a [3] = { 1.7, 2.2 };
-    float b [3] = { 1.0, 1.0 };
+    int dim = 3;
+    float a [dim] = { 1.7, 2.2, 3.3 };
+    float b [dim] = { 1.0, 1.0, 1.0 };
     float c = 0;
 
-    innerProduct(a, b, &c);
+    innerProduct(a, b, &c, &dim);
 
     cout << c << endl;
     
