304dbe2d7b867ac694a8e9301f426a2f847e25f4
Thomas Noel <thomas.c.noel@gmail.com>
2023-10-20 15:31:05 -0700

    Add (currently buggy) matrix multiplication

--- src/Linalg.cpp
+++ src/Linalg.cpp
@@ -1,1 +1,20 @@
-// TODO: Implement linear algebra utilities here!
+/*
+ *
+ * Linear algebra utilites
+ *
+ * Author: Thomas Noel
+ * Date: 10/20/23
+ */
+
+void innerProduct(float* x, float* y, float* out, int* dim)  {
+  // We pass in dim because sizeof(x) / sizeof(x[0]) doesn't work here..
+  // x is construed as the pointer pointing to x[0] in function scope
+  int N = *dim;
+
+  for (int i = 0; i < N; i++) {
+    *out += x[i] * y[i]; 
+  }
+}
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,143 @@
 
 using namespace std;
 
-void innerProduct(float* x, float* y, float* out, int* dim)  {
-    // We pass in dim because sizeof(x) / sizeof(x[0]) doesn't work here..
-    // x is construed as the pointer pointing to x[0] in function scope
-    int N = *dim;
+class Matrix {
 
-    for (int i = 0; i < N; i++) {
-        cout << *out << endl;
-        *out += x[i] * y[i]; 
+public:
+  size_t inSize;
+  size_t outSize;
+  float* data;
+
+  float* get(int row, int col) {
+    return &(data[row * outSize + col]);
+  }
+
+  void row(int rowNum, float* out) {
+    for (int i = 0; i < outSize; i++) {
+      out[i] = outSize * rowNum + i;
     }
+  }
+
+  void col(int colNum, float* out) {
+    for (int i = 0; i < inSize; i++) {
+      out[i] = outSize * i + colNum;
+    }
+  }
+
+  // void T() {
+  //   // Transpose the matrix
+  //   size_t holder = inSize;
+  //   inSize = outSize;
+  //   outSize = holder;
+
+  //   // Initialize and populate new data array
+
+  //   // assign new data array to data class member
+
+  //   // destroy old data array
+  // }
+
+  // TODO: slice, eigen, cholesky, rank, is_psd, etc..
+};
+
+void innerProduct(float* x, float* y, float* out, size_t dim)  {
+  // We pass in dim because sizeof(x) / sizeof(x[0]) doesn't work here..
+  // x is construed as the pointer pointing to x[0] in function scope
+  for (int i = 0; i < dim; i++) {
+    *out += x[i] * y[i]; 
+  }
 }
 
+void matMul(Matrix* A, Matrix* B, Matrix* out) {
+  if (A->outSize != B->inSize) {
+    throw "Invalid dimensions! Could not compute matrix product.";
+  }
+
+  size_t inDim = A->inSize;
+  size_t outDim = B->outSize;
+  size_t innerDim = A->outSize;
+  float row[A->outSize];
+  float col[B->inSize];
+
+  // Simple O(N^3) solution
+  for (int i = 0; i < inDim; i++) {
+    A->row(i, row);
+    for (int j = 0; j < outDim; j++) {
+      B->col(j, col);
+      innerProduct(row, col, out->get(i,j), innerDim);
+    }
+  }
+
+  out->inSize = inDim;
+  out->outSize = outDim;
+} 
+
 int main() {
-    int dim = 3;
-    float a [dim] = { 1.7, 2.2, 3.3 };
-    float b [dim] = { 1.0, 1.0, 1.0 };
-    float c = 0;
+  /************************************************/
+  // innerProduct test
+  /************************************************/
+  size_t dim = 3;
+  float a [3] = { 1.7, 2.2, 3.3 };
+  float b [3] = { 1.0, 1.0, 1.0 };
+  float c = 0;
 
-    innerProduct(a, b, &c, &dim);
+  innerProduct(a, b, &c, dim);
 
-    cout << c << endl;
-    
-    return 0;
+  if (c == 7.2) {
+    cout << "innerProduct passes" << endl;
+  } 
+
+  /************************************************/
+  // matMul test
+  /************************************************/
+  Matrix* A = new Matrix();
+  Matrix* B = new Matrix();
+  Matrix* C = new Matrix();
+
+  A->inSize = 2;
+  A->outSize = 3;
+  float dataA[6] = {2, 1, 4, 3, 2, 0.5};
+  A->data = dataA;
+
+  B->inSize = 3;
+  B->outSize = 2;
+  float dataB[6] = {1, 0.5, 2, 1, 3, 1};
+  B->data = dataB;
+
+  matMul(A, B, C);
+  float target[2][2] = { {16, 6}, {8.5, 4} };
+  bool passed = true;
+
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      if (*(C->get(i,j)) != target[i][j]) {
+        passed = false;
+      } 
+    }
+  }
+
+  if (passed) {
+    cout << "matMul passes" << endl;
+  } else {
+    cout << "matMul failed" << endl;
+    cout << "Expected: " << endl;
+    for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < 2; j++) {
+        cout << target[i][j] << ", ";
+      }
+    }
+    cout << target << endl;
+    cout << "Computed: " << endl;
+    for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < 2; j++) {
+        cout << *(C->get(i,j)) << ", ";
+      }
+    }
+  }
+
+  delete A;
+  delete B;
+  delete C;
+  
+  return 0;
 }
